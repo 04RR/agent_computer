eb27c0595447e1c554cdc7b5f3285a96173acdbb
Rohit Rajesh <rajesh.rohit04@gmail.com>
2026-03-01 00:22:15 +0530

    Update start_gateway.bat

--- start_gateway.bat
+++ start_gateway.bat
@@ -1,2 +1,2 @@
-SET OPENROUTER_API_KEY=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
+SET OPENROUTER_API_KEY=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 # this is an exmaple KEY!
 python gateway.py
